57cff2ce05c5ccf84e9a72bc337e80a5e99e5db5
Victor Barreto <v.barreto.and@gmail.com>
2020-03-25 09:32:56 -0300

    feat(server): develop some requisitions to make examples of parameters type

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const severPort = 3030;
+const successSeverUp = () =>
+  console.log(`\n\t>>> Sever starts with success on port ${severPort}`);
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/isAlive/:id/:age/:city", (request, response) => {
+  response.status(200).send(request.params);
+});
+
+app.get("/isAlive", (request, response) => {
+  response.status(200).send(request.query);
+});
+
+app.get("/isAliveBody", (request, response) => {
+  response.status(200).send(request.body);
+});
+
+app.listen(severPort, successSeverUp);
