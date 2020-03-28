56309b94d5e8556d28eb84beebb8278e744e5a8f
Victor Barreto <v.barreto.and@gmail.com>
2020-03-28 17:57:02 -0300

    feat(index): include cors and server changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const severPort = 3030;
 const successSeverUp = () =>
-  console.log(`\n\t>>> Sever starts with success on port ${severPort}`);
+  console.log(`\n\t>>> Sever running successfully on port ${severPort}\n`);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get("/isAlive/:id/:age/:city", (request, response) => {
-  response.status(200).send(request.params);
-});
-
-app.get("/isAlive", (request, response) => {
-  response.status(200).send(request.query);
-});
-
-app.get("/isAliveBody", (request, response) => {
-  response.status(200).send(request.body);
-});
-
 app.use(routes);
 
 app.listen(severPort, successSeverUp);
