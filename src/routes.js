35a0ca945cbb6e21f5c3885aba1d4e1fd748c2e4
Victor Barreto <v.barreto.and@gmail.com>
2020-03-27 23:45:11 -0300

    feat(routes): include new routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const OngController = require("./cotrollers/OngController");
 const IncidentController = require("./cotrollers/IncidentController");
+const ProfileController = require("./cotrollers/ProfileController");
+const SessionController = require("./cotrollers/SessionController");
 
 const routes = express.Router();
+
+routes.post("/session", SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
+routes.get("/profile", ProfileController.index);
+
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
 module.exports = routes;
